0ef0a0ae0cbd9a9d7109b8ce1f85b1f8a2ce4894
spider-Monty <montyaction@hotmail.com>
2023-01-31 18:59:28 +0530

    add className and some changes index.js file

--- src/index.js
+++ src/index.js
@@ -1,15 +1,31 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+// import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+class App extends React.Component {
+  render() {
+    return (
+      <div className='App'>
+        <h1>Hello {name}!</h1>
+      </div>
+    );
+  }
+}
+const name = 'React';
+// JSX
+const heading = <h1 className="site-heading">Hello, React!</h1>
+// No JSX
+const heading1 = React.createElement('h1', { className : 'site-heading'}, 'Hello, React!')
+ReactDOM.render(<App />, document.getElementById('root'))
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
