65bd1166fcf3936a2dd96aceebbbe2893ed0a2cd
spider-Monty <montyaction@hotmail.com>
2023-02-21 13:35:11 +0530

    Pass an array App.js to list.js component

--- src/Components/list.js
+++ src/Components/list.js
@@ -1,20 +1,43 @@
 import React from 'react'
 
-function list() {
+// class list extends React.Component {
+  // constructor(props) {
+  //   super(props);
+  // }
+  
+class list extends React.Component {
+  
+  state = {
+    user : [
+      {id : 1, name : "ArunDhati", age : 25, class : "C"},
+      {id : 2, name : "Munnka", age : 21, class : "A"},
+      {id : 3, name : "Dhati", age : 22, class : "D"},
+      {id : 4, name : "Arun", age : 24, class : "B"},
+    ]
+  };
+  
+  render() {
+    const users = this.state.user.map(user=><li key={user.id}>{user.id} {user.name} Age - {user.age} Class - {user.class}</li>);
 
-    const numbers = [3,4,5,6];
+    console.log(this.state.user);
+
+    const numbers = this.props.numbers;
     // const newNumbers = numbers.map((numbers)=><li>{numbers}</li>);
+    console.log(numbers);
 
-  return (
-    <div>
-        <h1>List</h1>
-        {/* <ul>{newNumbers}</ul> */}
-        {
-            numbers.map((numbers)=><li>{numbers}</li>)
-        }
-        <h2>{numbers[0]}</h2>
-    </div>
-  );
+        return (
+          
+          <div>
+              <h1>List</h1>
+              {/* <ul>{newNumbers}</ul> */}
+              {
+                  numbers.map((numbers)=><li key={numbers}>{numbers}</li>)
+              }
+              <h2>{numbers[0]}</h2>
+              <ul>{users}</ul>
+          </div>
+        );
+      }
 }
 
 export default list
